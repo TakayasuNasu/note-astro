e10a4149cf718f6d2a705a2e14eecbbc70508ff3
takayasu.nasu <taka.beckham@gmail.com>
2023-04-12 02:29:37 +0000

    Add Setting of Vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,11 @@
 import { defineConfig } from 'astro/config';
+import react from "@astrojs/react";
+
+import vercel from "@astrojs/vercel/edge";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [react()],
+  output: "server",
+  adapter: vercel()
+});
