318e01c751ed3d5f53b740b06d8b2ba1fa61b191
takayasu.nasu <taka.beckham@gmail.com>
2023-07-22 06:44:20 +0000

    Add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()],
+  site: 'https://blog.i-nasu.com',
+  integrations: [react(), tailwind(), sitemap()],
   experimental: {
     assets: true
-   },
+  }
 });
